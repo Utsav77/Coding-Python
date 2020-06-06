5b917bea0eaa51967b5be8254895f21606ebb6fc
Utsab Kumar Agrawal <52622694+Utsav77@users.noreply.github.com>
2020-06-06 22:46:52 +0530

    Add files via upload

---
+++ queue_reconstruction_height.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        a=[]
+        people.sort(key = lambda a: (-a[0], a[1]))
+        for i in people:
+            a.insert(i[1],i)
+        return a
