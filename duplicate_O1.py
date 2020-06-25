808e036efe7f0ff855a3fb895f55059ecc0470e6
Utsab Kumar Agrawal <52622694+Utsav77@users.noreply.github.com>
2020-06-25 17:21:36 +0530

    Add files via upload

---
+++ duplicate_O1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        tor = hare = nums[0]
+        
+        while True:
+            tor = nums[tor]
+            hare = nums[nums[hare]]
+            if tor == hare:
+                break
+                
+        tor = nums[0]
+        
+        while tor != hare:
+            tor = nums[tor]
+            hare = nums[hare]
+        return hare
